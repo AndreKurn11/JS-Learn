4620d5b178c51f4113f7ff90ff3d94204a33535f
AndreKurn11 <muhammadandre110311@gmail.com>
2026-01-20 12:50:16 +0700

    trivia bot add

---
+++ trivia-bot.js
@@ -0,0 +1,19 @@
+console.log("Hello! I'm your coding fun fact guide!");
+
+const botName ="Dre";
+const botLocation = "Earth";
+const favoriteLanguage = "TypeScript";
+
+console.log("My name is " + botName + " and I live on " + botLocation + ".");
+console.log("My favorite programming language is " + favoriteLanguage + ".");
+
+let codingFact = favoriteLanguage + " is most used language";
+console.log(codingFact);
+
+codingFact = favoriteLanguage + " same as JavaScript";
+console.log(codingFact);
+
+codingFact = favoriteLanguage + " has a cool logo";
+console.log(codingFact);
+
+console.log("It was fun sharing these facts with you. Goodbye! - " + botName + " from " +   botLocation + ".");
